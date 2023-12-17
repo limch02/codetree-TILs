9115e22a786dd4d135d82c2f622f63def849235a
limch02 <144610436+limch02@users.noreply.github.com>
2023-12-17 18:21:21 +0900

    Title: 출석 부르기, Time: 105ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 231217/출석 부르기/calling-attendance.java
@@ -0,0 +1,22 @@
+import javax.swing.*;
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int a = Integer.parseInt(br.readLine());
+        if (a==1){
+            bw.write("John");
+        } else if (a==2) {
+            bw.write("Tom");
+        } else if (a==2){
+            bw.write("paul");
+        }else {
+            bw.write("Vacancy");
+        }
+        bw.close();
+    }
+}
