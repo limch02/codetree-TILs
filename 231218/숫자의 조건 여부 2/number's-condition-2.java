ae614626ff6b478f9f005dda4515514143e63c57
limch02 <144610436+limch02@users.noreply.github.com>
2023-12-18 01:17:19 +0900

    Title: 숫자의 조건 여부 2, Time: 91ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 231218/숫자의 조건 여부 2/number's-condition-2.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int a = Integer.parseInt(br.readLine());
+        if (a%5==0){
+            bw.write("A");
+        }
+        if (a%2==0){
+            bw.write("B");
+        }
+        bw.close();
+    }
+}
