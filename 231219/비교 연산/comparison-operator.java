d2cb164332abc0103ec8434e40ec6861d023d4aa
limch02 <144610436+limch02@users.noreply.github.com>
2023-12-19 12:41:02 +0900

    Title: 비교 연산, Time: 94ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 231219/비교 연산/comparison-operator.java
@@ -0,0 +1,43 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb= new StringBuilder();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        if (a>=b){
+            sb.append("1\n");
+        }else {
+            sb.append("0\n");
+        }
+        if (a>b){
+            sb.append("1\n");
+        }else {
+            sb.append("0\n");
+        }
+        if (a<=b){
+            sb.append("1\n");
+        }else {
+            sb.append("0\n");
+        }
+        if (a<b){
+            sb.append("1\n");
+        }else {
+            sb.append("0\n");
+        }
+        if (a==b){
+            sb.append("1\n");
+        }else {
+            sb.append("0\n");
+        }
+        if (a!=b){
+            sb.append("1\n");
+        }else {
+            sb.append("0\n");
+        }
+        System.out.print(sb);
+    }
+}
