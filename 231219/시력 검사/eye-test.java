727b2f6f45892a4568c0203d0b15138b045b4453
limch02 <144610436+limch02@users.noreply.github.com>
2023-12-19 12:49:45 +0900

    Title: 시력 검사, Time: 95ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 231219/시력 검사/eye-test.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb= new StringBuilder();
+        double a = Double.parseDouble(br.readLine());
+        double b = Double.parseDouble(br.readLine());
+        if (a>=1.0&&b>=1.0){
+            sb.append("high");
+        } else if (a>=0.5&&b>=0.5) {
+            sb.append("middle");
+        } else {
+            sb.append("low");
+        }
+        System.out.print(sb);
+    }
+}
