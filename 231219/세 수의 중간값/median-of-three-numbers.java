527ddcaa3802e32add094065888fb05b12d6f04e
limch02 <144610436+limch02@users.noreply.github.com>
2023-12-19 12:57:10 +0900

    Title: 세 수의 중간값, Time: 107ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 231219/세 수의 중간값/median-of-three-numbers.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
+        if(b>a&&b<c){
+            sb.append("1");
+        }else{
+            sb.append("0");
+        }
+        System.out.print(sb);
+    }
+}
