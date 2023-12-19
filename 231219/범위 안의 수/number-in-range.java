b5cd1bf2bc5a4c6bb0f2a1b2308d4b1e86836ede
limch02 <144610436+limch02@users.noreply.github.com>
2023-12-19 12:46:51 +0900

    Title: 범위 안의 수, Time: 96ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 231219/범위 안의 수/number-in-range.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb= new StringBuilder();
+        int a = Integer.parseInt(br.readLine());
+        if (a>=10&&a<=20){
+            sb.append("yes");
+        }else {
+            sb.append("no");
+        }
+        System.out.print(sb);
+    }
+}
