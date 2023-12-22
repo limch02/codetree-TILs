808158e3c934f5fd152e4569fc9a2855a7889324
limch02 <144610436+limch02@users.noreply.github.com>
2023-12-22 16:56:19 +0900

    Title: 두 수의 거듭제곱, Time: 94ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 231222/두 수의 거듭제곱/two-numbers-of-squares.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static int num(int n, int m){
+        return (int) Math.pow(n,m);
+    }
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        System.out.println(num(a,b));
+    }
+}
