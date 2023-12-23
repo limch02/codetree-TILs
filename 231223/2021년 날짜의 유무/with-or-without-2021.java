71008216752dba113cb4847cb6dab44cec8550af
limch02 <144610436+limch02@users.noreply.github.com>
2023-12-23 14:24:54 +0900

    Title: 2021년 날짜의 유무, Time: 109ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 231223/2021년 날짜의 유무/with-or-without-2021.java
@@ -0,0 +1,42 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        if (a ==2){
+            if (b<29){
+                System.out.println("Yes");
+            }else {
+                System.out.println("No");
+            }
+        } else if (a<8&&a%2==0){
+            if (b<31){
+                System.out.println("Yes");
+            }else {
+                System.out.println("No");
+            }
+        } else if (a<=7&&a%2==1) {
+            if (b<32){
+                System.out.println("Yes");
+            }else {
+                System.out.println("No");
+            }
+        } else if (a>=8&&a%2==0) {
+            if (b<32){
+                System.out.println("Yes");
+            }else {
+                System.out.println("No");
+            }
+        } else if (a>7&&a%2==1) {
+            if (b<31){
+                System.out.println("Yes");
+            }else {
+                System.out.println("No");
+            }
+        }
+    }
+}
