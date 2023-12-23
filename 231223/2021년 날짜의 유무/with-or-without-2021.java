720091143976a3315c15bde15a6dbf2705d5756e
limch02 <144610436+limch02@users.noreply.github.com>
2023-12-23 14:26:35 +0900

    Title: 2021년 날짜의 유무, Time: 103ms, Memory: 8MB, Status: Passed - CodeTree

--- 231223/2021년 날짜의 유무/with-or-without-2021.java
+++ 231223/2021년 날짜의 유무/with-or-without-2021.java
@@ -37,6 +37,8 @@ public class Main {
             }else {
                 System.out.println("No");
             }
-        } 
+        } else {
+            System.out.println("No");
+        }
     }
 }
