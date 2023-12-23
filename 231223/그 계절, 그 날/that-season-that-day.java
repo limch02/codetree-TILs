e5a2bfef91487b9e3aeecfab1e0423c09bd14523
limch02 <144610436+limch02@users.noreply.github.com>
2023-12-23 14:38:45 +0900

    Title: 그 계절, 그 날, Time: 89ms, Memory: 8MB, Status: Wrong Answer - CodeTree

--- 231223/그 계절, 그 날/that-season-that-day.java
+++ 231223/그 계절, 그 날/that-season-that-day.java
@@ -1,140 +1,42 @@
-import java.util.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static boolean num(int n){
-        if (n%4==0&&n%100==0&&n%400==0){
-            return true;
-        } else if (n%4==0&&n%100==0) {
-            return  false;
-        } else if (n%4==0) {
-            return true;
-        }else {
-            return false;
-        }
+    public static boolean isLeapYear(int n) {
+        return (n % 4 == 0 && (n % 100 != 0 || n % 400 == 0));
     }
-    public static void main(String[] args) throws Exception{
+
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         int y = Integer.parseInt(st.nextToken());
         int a = Integer.parseInt(st.nextToken());
         int b = Integer.parseInt(st.nextToken());
-        if (num(y)){
-            if (a ==2){
-                if (b<30){
+
+        // 각 달의 일수
+        int[] daysInMonth = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+
+        if (a >= 1 && a <= 12 && b >= 1 && b <= daysInMonth[a]) {
+            if (a == 2 && isLeapYear(y)) {
+                if (b <= 29) {
                     System.out.println("Winter");
-                }else {
-                    System.out.println("-1");
-                }
-            } else if (a<8&&a%2==0){
-                if (a<6){
-                    if (b<31){
-                        System.out.println("Spring");
-                    }else {
-                        System.out.println("-1");
-                    }
-                }else {
-                    if (b<31){
-                        System.out.println("Summer");
-                    }else {
-                        System.out.println("-1");
-                    }
-                }
-            } else if (a<=7&&a%2==1) {
-                if (a==7){
-                    if (b<32){
-                        System.out.println("Summer");
-                    }else {
-                        System.out.println("-1");
-                    }
-                }else{
-                    if (b<32){
-                        System.out.println("Spring");
-                    }else {
-                        System.out.println("-1");
-                    }
-                }
-            } else if (a>=8&&a%2==0&&a<13) {
-                if (a<11) {
-                    if (b < 32) {
-                        System.out.println("Fall");
-                    } else {
-                        System.out.println("-1");
-                    }
-                }else {
-                    if (b < 32) {
-                        System.out.println("Winter");
-                    } else {
-                        System.out.println("-1");
-                    }
-                }
-            } else if (a>7&&a%2==1&&a<13) {
-                if (b<31){
-                    System.out.println("Fall");
-                }else {
+                } else {
                     System.out.println("-1");
                 }
             } else {
-                System.out.println("-1");
-            }
-        }else {
-            if (a ==2){
-                if (b<29){
-                    System.out.println("Winter");
-                }else {
-                    System.out.println("-1");
-                }
-            } else if (a<8&&a%2==0){
-                if (a<6){
-                    if (b<31){
-                        System.out.println("Spring");
-                    }else {
-                        System.out.println("-1");
-                    }
-                }else {
-                    if (b<31){
-                        System.out.println("Summer");
-                    }else {
-                        System.out.println("-1");
-                    }
-                }
-            } else if (a<=7&&a%2==1) {
-                if (a==7){
-                    if (b<32){
-                        System.out.println("Summer");
-                    }else {
-                        System.out.println("-1");
-                    }
-                }else{
-                    if (b<32){
-                        System.out.println("Spring");
-                    }else {
-                        System.out.println("-1");
-                    }
-                }
-            } else if (a>=8&&a%2==0&&a<13) {
-                if (a<11) {
-                    if (b < 32) {
-                        System.out.println("Fall");
-                    } else {
-                        System.out.println("-1");
-                    }
-                }else {
-                    if (b < 32) {
-                        System.out.println("Winter");
-                    } else {
-                        System.out.println("-1");
-                    }
-                }
-            } else if (a>7&&a%2==1&&a<13) {
-                if (b<31){
+                if (a >= 3 && a <= 5) {
+                    System.out.println("Spring");
+                } else if (a >= 6 && a <= 8) {
+                    System.out.println("Summer");
+                } else if (a >= 9 && a <= 11) {
                     System.out.println("Fall");
-                }else {
-                    System.out.println("-1");
+                } else {
+                    System.out.println("Winter");
                 }
-            } else {
-                System.out.println("-1");
             }
+        } else {
+            System.out.println("-1");
         }
     }
 }
