825dad2c555c019739afb120e551315a21e415f6
limch02 <144610436+limch02@users.noreply.github.com>
2023-12-28 22:09:01 +0900

    Title: k번째로 신기한 문자열, Time: 99ms, Memory: 9MB, Status: Passed - CodeTree

--- 231228/k번째로 신기한 문자열/kth-special-string.java
+++ 231228/k번째로 신기한 문자열/kth-special-string.java
@@ -1,26 +1,42 @@
 import java.util.*;
 import java.io.*;
 public class Main {
-    public static void main(String[] args) throws IOException{
+    static int n, k;
+    static String T;
+    static String[] arr;
+    public static void main(String[] args) throws IOException {
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int a = Integer.parseInt(st.nextToken());
-        int b = Integer.parseInt(st.nextToken());
-        String s = st.nextToken();
-        ArrayList<String> arr = new ArrayList<>();
-        for (int i =0;i<a;i++){
-            String s1 = br.readLine();
-            boolean b1 = true;
-            for (int j=0;j<s.length();j++){
-                if (s.charAt(j)!=s1.charAt(j)){
-                    b1 = false;
+
+        n = Integer.parseInt(st.nextToken());
+        k = Integer.parseInt(st.nextToken());
+        T = st.nextToken();
+
+        arr = new String[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = br.readLine();
+        }
+        Arrays.sort(arr);
+
+        A: for (int i = 0; i < n; i++) {
+            int cnt = 0;
+            for (int j = 0; j < T.length(); j++) {
+                if (T.length() > arr[i].length()) continue A;
+
+                if (arr[i].charAt(j) == T.charAt(j)) {
+                    cnt++;
+                }
+
+                if (cnt == T.length()) {
+                    k--;
+                }
+
+                if (k == 0) {
+                    System.out.println(arr[i]);
+                    return;
                 }
             }
-            if (b1) {
-                arr.add(s1);
-            }
         }
-        Collections.sort(arr);
-        System.out.print(arr.get(b-1));
     }
 }
