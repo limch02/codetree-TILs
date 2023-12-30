630bbfcbd6e82d0a4c2a6596bceed11afb56950c
limch02 <144610436+limch02@users.noreply.github.com>
2023-12-30 15:10:14 +0900

    Title: 비오는 날, Time: 90ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 231230/비오는 날/rainy-day.java
@@ -0,0 +1,34 @@
+import java.util.*;
+import java.io.*;
+public class Main {
+    static class lv{
+        String name, place,lv;
+        public lv(String name, String lv,String place){
+            this.name = name;
+            this.lv = lv;
+            this.place = place;
+        }
+    }
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int a = Integer.parseInt(br.readLine());
+        lv[] lvs = new lv[a];
+        int j =0;
+        String min = "9999999999999999999";
+        for (int i =0;i<a;i++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            String name = st.nextToken();
+            String l = (st.nextToken());
+            String p = st.nextToken();
+            if (p.equals("Rain")) {
+                if (name.compareTo(min) < 0) {
+                    min = name;
+                    j = i;
+                }
+            }
+            lvs[i] = new lv(name,l,p);
+        }
+        lv ss = lvs[j];
+        System.out.print(ss.name+" "+ss.lv+" "+ss.place);
+    }
+}
