23c70e7d65c77d2b7336155140582d78eed9fc44
limch02 <144610436+limch02@users.noreply.github.com>
2024-01-05 23:44:43 +0900

    Title: 구역 청소, Time: 103ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240105/구역 청소/district-cleaning.java
@@ -0,0 +1,42 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        StringTokenizer s = new StringTokenizer(br.readLine());
+        int c = Integer.parseInt(s.nextToken());
+        int d = Integer.parseInt(s.nextToken());
+        if (a==c){
+            if (b<d){
+                System.out.println(d-a);
+            }else {
+                System.out.println(b-a);
+            }
+        } else if(a<c){
+            if (b==c){
+                System.out.println(d-a);
+            } else if (b<c) {
+                System.out.println((b-a)+(d-c));
+            } else{
+                System.out.println(d-a);
+            }
+        }else{
+            if (d==a){
+                System.out.println(b-c);
+            } else if (d<a) {
+                System.out.println((b-a)+(d-c));
+            } else{
+                System.out.println(b-c);
+            }
+        }
+    }
+
+}
