959fbb0f12e256ec48d87fe789c76690146b5338
limch02 <144610436+limch02@users.noreply.github.com>
2024-01-07 15:21:11 +0900

    Title: 일 수 구하기, Time: 95ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240107/일 수 구하기/number-of-days-in-month.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int a = Integer.parseInt(br.readLine());
+        int [] arr = {31,28,31,30,31,30,31,31,30,31,30,31};
+        sb.append(arr[a-1]);
+        System.out.println(sb);
+    }
+}
