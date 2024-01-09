0692ed5777c45f8e51c97073570fedb78aca65e3
limch02 <144610436+limch02@users.noreply.github.com>
2024-01-09 13:35:05 +0900

    Title: 실수 입력받아 계산 2, Time: 154ms, Memory: 9MB, Status: Passed - CodeTree

--- 240109/실수 입력받아 계산 2/calculation-by-inputting-a-real-number-2.java
+++ 240109/실수 입력받아 계산 2/calculation-by-inputting-a-real-number-2.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         double a = Double.parseDouble(br.readLine());
-        String s = String.format("%.2f",a*1.5);
+        String s = String.format("%.2f",a+1.5);
         System.out.print(s);
     }
 }
