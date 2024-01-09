736a9d67fcfec4dca021bae4b691adff73aaa9c5
limch02 <144610436+limch02@users.noreply.github.com>
2024-01-09 13:33:19 +0900

    Title: 실수 받아 그대로 출력, Time: 102ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 240109/실수 받아 그대로 출력/enter-real-value-and-print.java
@@ -0,0 +1,10 @@
+import java.util.*;
+import java.io.*;
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        double a = Double.parseDouble(br.readLine());
+        String s = String.format("%.2f",a);
+        System.out.print(s);
+    }
+}
