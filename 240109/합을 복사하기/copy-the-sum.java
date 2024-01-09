4be0aec216f9790452da1cd903a1080914ba718b
limch02 <144610436+limch02@users.noreply.github.com>
2024-01-09 13:27:54 +0900

    Title: 합을 복사하기, Time: 88ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240109/합을 복사하기/copy-the-sum.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.io.*;
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int a = 1;
+        int b = 2;
+        int c = 3;
+        c = 6;
+        a = c;
+        b =c;
+        System.out.println(a+" "+b+" "+c);
+    }
+}
