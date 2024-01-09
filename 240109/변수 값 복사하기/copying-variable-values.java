9e813b9176540d74a26d5565e5d84a4111479a1b
limch02 <144610436+limch02@users.noreply.github.com>
2024-01-09 13:26:31 +0900

    Title: 변수 값 복사하기, Time: 90ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240109/변수 값 복사하기/copying-variable-values.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.io.*;
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int a = 1;
+        int b = 2;
+        int c = 3;
+        a = c;
+        b =c;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+    }
+}
