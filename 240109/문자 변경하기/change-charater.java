864084976449f8b47556d02748a1ae6f1f991315
limch02 <144610436+limch02@users.noreply.github.com>
2024-01-09 13:20:23 +0900

    Title: 문자 변경하기, Time: 107ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240109/문자 변경하기/change-charater.java
@@ -0,0 +1,10 @@
+import java.util.*;
+import java.io.*;
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        char a = 'C';
+        a = 'T';
+        System.out.println(a);
+    }
+}
