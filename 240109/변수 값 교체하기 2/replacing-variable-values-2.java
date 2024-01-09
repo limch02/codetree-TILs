c67f8d0ea078270b39f03354456c8fd681cc40fb
limch02 <144610436+limch02@users.noreply.github.com>
2024-01-09 13:21:12 +0900

    Title: 변수 값 교체하기 2, Time: 107ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240109/변수 값 교체하기 2/replacing-variable-values-2.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.io.*;
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int a = 5;
+        int b = 3;
+        a = b;
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
