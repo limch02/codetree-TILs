c909a3640b60de9a86f5fb463be505781ec1bf6c
limch02 <144610436+limch02@users.noreply.github.com>
2024-01-09 13:29:51 +0900

    Title: 입력받아 계산, Time: 92ms, Memory: 8MB, Status: Passed - CodeTree

--- 240109/입력받아 계산/input-calculate.java
+++ 240109/입력받아 계산/input-calculate.java
@@ -3,7 +3,7 @@ import java.io.*;
 public class Main {
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int a = Interger.parseInt(br.readLine());
+        int a = Integer.parseInt(br.readLine());
         System.out.println(a+2);
     }
 }
