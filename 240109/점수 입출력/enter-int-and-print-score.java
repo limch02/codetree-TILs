a808dfb9a05523b0f7f562783af46a7ba58005b9
limch02 <144610436+limch02@users.noreply.github.com>
2024-01-09 13:30:46 +0900

    Title: 점수 입출력, Time: 93ms, Memory: 8MB, Status: Passed - CodeTree

--- 240109/점수 입출력/enter-int-and-print-score.java
+++ 240109/점수 입출력/enter-int-and-print-score.java
@@ -4,6 +4,6 @@ public class Main {
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int a = Integer.parseInt(br.readLine());
-        System.out.println("Your score is "+a+"point.");
+        System.out.println("Your score is "+a+" point.");
     }
 }
