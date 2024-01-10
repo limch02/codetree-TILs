0010810b33cea494f186d83cf7fb191047b05343
limch02 <144610436+limch02@users.noreply.github.com>
2024-01-10 13:36:06 +0900

    Title: 합과 차의 나눗셈, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240110/합과 차의 나눗셈/divide-of-sum-and-sub.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        String s = String.format("%.2f",(a+b)/(a-b));
+    }
+}
