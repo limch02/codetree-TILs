80a2de856e786cd6626582de4b20983905be62b1
limch02 <144610436+limch02@users.noreply.github.com>
2024-01-10 13:26:05 +0900

    Title: 전화번호 바꾸기, Time: 102ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240110/전화번호 바꾸기/change-phone-number.java
@@ -0,0 +1,8 @@
+import java.io.*;
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[]arr = br.readLine().split("-");
+        System.out.println(arr[0]+"-"+arr[2]+"-"+arr[1]);
+    }
+}
