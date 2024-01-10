d7bf45f24f6d7a323ac3b8c91ae596eb4e8a317f
limch02 <144610436+limch02@users.noreply.github.com>
2024-01-10 13:25:30 +0900

    Title: 날짜 변경하여 출력, Time: 107ms, Memory: 8MB, Status: Passed - CodeTree

--- 240110/날짜 변경하여 출력/print-date-with-different-format.java
+++ 240110/날짜 변경하여 출력/print-date-with-different-format.java
@@ -2,7 +2,7 @@ import java.io.*;
 public class Main {
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String[]arr = br.readLine().split(".");
+        String[]arr = br.readLine().split("\\.");
         System.out.println(arr[1]+"-"+arr[2]+"-"+arr[0]);
     }
 }
