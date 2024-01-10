8d3051014623a40f198946b270f81b703442e3d3
limch02 <144610436+limch02@users.noreply.github.com>
2024-01-10 13:18:55 +0900

    Title: 문자열 순서 바꾸기, Time: 92ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240110/문자열 순서 바꾸기/change-order-of-strings.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.io.*;
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        String s = st.nextToken();
+        String k = br.readLine();
+        System.out.println(k);
+        System.out.print(s);
+    }
+}
