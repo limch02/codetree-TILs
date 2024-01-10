733a339a25b088a9e962281991de917fb5973c11
limch02 <144610436+limch02@users.noreply.github.com>
2024-01-10 13:32:26 +0900

    Title: 입력받아 연산하기, Time: 88ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240110/입력받아 연산하기/take-input-and-operate.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(br.readLine());
+        System.out.println(a+87);
+        System.out.println(b%10);
+    }
+}
