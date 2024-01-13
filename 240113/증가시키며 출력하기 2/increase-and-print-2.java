1808170403097289304af0c3b032e36a31cac901
limch02 <144610436+limch02@users.noreply.github.com>
2024-01-13 14:01:59 +0900

    Title: 증가시키며 출력하기 2, Time: 90ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240113/증가시키며 출력하기 2/increase-and-print-2.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int i = 10;
+        while (i<27){
+            System.out.print(i+" ");
+            i++;
+        }
+    }
+}
