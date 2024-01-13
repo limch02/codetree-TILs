9480e1516bc668c7d839351bcfd5eb069c19002a
limch02 <144610436+limch02@users.noreply.github.com>
2024-01-13 14:20:14 +0900

    Title: 문자 출력하기, Time: 92ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240113/문자 출력하기/print-text.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s = br.readLine();
+        for (int i = 0 ; i<8;i++){
+            System.out.print(s);
+        }
+    }
+}
