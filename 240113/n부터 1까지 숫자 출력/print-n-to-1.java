64fe26d8474668bef17102cabee162758a1c1323
limch02 <144610436+limch02@users.noreply.github.com>
2024-01-13 14:18:39 +0900

    Title: n부터 1까지 숫자 출력, Time: 102ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240113/n부터 1까지 숫자 출력/print-n-to-1.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int i = Integer.parseInt(st.nextToken());
+        while (i>0){
+            System.out.print(i + " ");
+            i--;
+        }
+    }
+}
