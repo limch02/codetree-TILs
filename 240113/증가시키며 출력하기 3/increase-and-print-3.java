a3e8a60b273a32f12bf6383b4ca1fad2ba9503f9
limch02 <144610436+limch02@users.noreply.github.com>
2024-01-13 14:06:07 +0900

    Title: 증가시키며 출력하기 3, Time: 96ms, Memory: 8MB, Status: Passed - CodeTree

--- 240113/증가시키며 출력하기 3/increase-and-print-3.java
+++ 240113/증가시키며 출력하기 3/increase-and-print-3.java
@@ -5,10 +5,9 @@ public class Main {
     public static void main(String args[]) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
-        int a = Integer.parseInt(br.readLine());
         int i = 10;
         while (i<=26){
-            System.out.println(i+" ");
+            System.out.print(i+" ");
             i+=2;
         }
     }
