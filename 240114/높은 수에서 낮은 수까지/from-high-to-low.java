811583b1cc7dad57aea16ee35a654c2243439216
limch02 <144610436+limch02@users.noreply.github.com>
2024-01-14 15:46:24 +0900

    Title: 높은 수에서 낮은 수까지, Time: 86ms, Memory: 8MB, Status: Wrong Answer - CodeTree

--- 240114/높은 수에서 낮은 수까지/from-high-to-low.java
+++ 240114/높은 수에서 낮은 수까지/from-high-to-low.java
@@ -11,10 +11,12 @@ public class Main {
             for (int i =  a;i>=b;i--){
                 System.out.print(i+" ");
             }
-        }else {
+        }else if (a<b){
             for (int i = b;i>a;i--){
                 System.out.print(i+" ");
             }
+        }else{
+            System.out.print(a);
         }
     }
 }
