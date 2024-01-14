1458a782fa31565add82d265932ef82be43e1de9
limch02 <144610436+limch02@users.noreply.github.com>
2024-01-14 15:45:36 +0900

    Title: 높은 수에서 낮은 수까지, Time: 87ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240114/높은 수에서 낮은 수까지/from-high-to-low.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        if (a>b){
+            for (int i =  a;i>=b;i--){
+                System.out.print(i+" ");
+            }
+        }else {
+            for (int i = b;i>a;i--){
+                System.out.print(i+" ");
+            }
+        }
+    }
+}
