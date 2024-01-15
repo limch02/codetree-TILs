c3f0f5ba394d7fb75e44d027fb15db3358f381c4
limch02 <144610436+limch02@users.noreply.github.com>
2024-01-15 14:46:37 +0900

    Title: 범위 안의 수 2, Time: 100ms, Memory: 9MB, Status: Passed - CodeTree

--- 240115/범위 안의 수 2/number-in-range-2.java
+++ 240115/범위 안의 수 2/number-in-range-2.java
@@ -4,7 +4,6 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st= new StringTokenizer(br.readLine());
         double sum = 0;
         double cnt = 0;
         for (int i =1; i <=10;i++){
