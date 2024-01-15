54c4e8aacc3dd7df951b94ea33f32b3fb5fe91fa
limch02 <144610436+limch02@users.noreply.github.com>
2024-01-15 14:23:59 +0900

    Title: 청소당번 정하기, Time: 92ms, Memory: 8MB, Status: Passed - CodeTree

--- 240115/청소당번 정하기/cleaning-numbering.java
+++ 240115/청소당번 정하기/cleaning-numbering.java
@@ -9,14 +9,12 @@ public class Main {
         int bcnt = 0;
         int ccnt = 0;
         for (int i = 1; i<=a;i++){
-            if (i%2==0){
-                acnt++;
-            }
-            if (i%3==0){
-                bcnt++;
-            }
             if (i % 12 == 0) {
                 ccnt++;
+            } else if (i%3==0) {
+                bcnt++;
+            } else if (i%2==0) {
+                acnt++;
             }
         }
         System.out.print(acnt+" "+bcnt+" "+ccnt);
