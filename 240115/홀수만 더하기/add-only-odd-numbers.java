443fc0fad2ab978dfb6d54da8dedd7ee9260ca08
limch02 <144610436+limch02@users.noreply.github.com>
2024-01-15 14:34:11 +0900

    Title: 홀수만 더하기, Time: 91ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240115/홀수만 더하기/add-only-odd-numbers.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st= new StringTokenizer(br.readLine());
+        int a = Integer.parseInt(st.nextToken());
+        int sum = 0;
+        for (int i =1; i <=a;i++){
+            int b = Integer.parseInt(br.readLine());
+            if (b%2==1){
+                sum+=b;
+            }
+        }
+        System.out.println(sum);
+    }
+}
