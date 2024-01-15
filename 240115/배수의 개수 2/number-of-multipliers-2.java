e0a85d5b1dabdbd9aea91a2a62dad8377dff8305
limch02 <144610436+limch02@users.noreply.github.com>
2024-01-15 14:17:56 +0900

    Title: 배수의 개수 2, Time: 92ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240115/배수의 개수 2/number-of-multipliers-2.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int cnt = 0;
+        for (int i = 0; i<10;i++){
+            int a = Integer.parseInt(br.readLine());
+            if (a%2==1){
+                cnt++;
+            }
+        }
+        System.out.print(cnt);
+    }
+}
