217ea2510cda5096773ed39a3b6559e74df2909b
limch02 <144610436+limch02@users.noreply.github.com>
2024-01-16 13:46:06 +0900

    Title: 조건에 부합하는 수, Time: 89ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240116/조건에 부합하는 수/number-that-meets-the-condition.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int a = Integer.parseInt(st.nextToken());
+        int s = 0;
+        for (int i = 1;i<=a;i++){
+            if (i%2==0&i%4!=0){
+                continue;
+            } else if ((i/8)%2==0) {
+                continue;
+            } else if (i%7>=4) {
+                continue;
+            }else {
+                System.out.print(i+" ");
+            }
+        }
+    }
+}
