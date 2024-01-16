5e51195519d5fc7b51b6b980a81708fb0de42af7
limch02 <144610436+limch02@users.noreply.github.com>
2024-01-16 13:51:25 +0900

    Title: 1부터의 합, Time: 91ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240116/1부터의 합/sum-from-the-one.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int a = Integer.parseInt(st.nextToken());
+        int s = 0;
+        int k = 0;
+        for (int i = 1;i<=a;i++){
+            if (s>=a){
+                break;
+            }
+            s+=i;
+            k =i;
+        }
+        System.out.println(k);
+    }
+}
