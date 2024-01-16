860776c413d81ff33bbbb58641c3536b564aa072
limch02 <144610436+limch02@users.noreply.github.com>
2024-01-16 14:25:05 +0900

    Title: 0 나올때까지 출력, Time: 103ms, Memory: 8MB, Status: Passed - CodeTree

--- 240116/0 나올때까지 출력/print-until-zero-is-given.java
+++ 240116/0 나올때까지 출력/print-until-zero-is-given.java
@@ -4,21 +4,13 @@ import java.io.*;
 public class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int a = Integer.parseInt(st.nextToken());
-        int k = 0;
-
-        int originalA = a;  // 입력값을 보존하기 위한 변수
-
-        for (int i = 1; i <= originalA; i++) {
-            a = a / i;
-
-            if (a <= 1) {
-                k = i;
+        while(true){
+            int a = Integer.parseInt(br.readLine());
+            if(a==0){
                 break;
+            }else{
+                System.out.println(a);
             }
         }
-
-        System.out.print(k);
     }
 }
