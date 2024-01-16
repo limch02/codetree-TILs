f5f72eddee5c272303aec0d52389594ac50de467
limch02 <144610436+limch02@users.noreply.github.com>
2024-01-16 14:32:37 +0900

    Title: 출석 부르기 2, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240116/출석 부르기 2/calling-attendance-2.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        while(true){
+            int a = Integer.parseInt(br.readLine());
+            if(a==1){
+                System.out.println("John");
+            }else if(a==2){
+                System.out.println("Tom");
+            } else if(a==3){
+                System.out.println("Paul"));
+            } else if(a==4){
+                System.out.println("Sam"));
+            } else{
+                System.out.println("Vaancy");
+                break;
+            }
+        }
+    }
+}
