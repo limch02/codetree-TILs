93bb3ad8dc13826b2d454e4fe0f603c4b98e5a9e
limch02 <144610436+limch02@users.noreply.github.com>
2024-01-16 14:32:49 +0900

    Title: 출석 부르기 2, Time: 99ms, Memory: 8MB, Status: Wrong Answer - CodeTree

--- 240116/출석 부르기 2/calling-attendance-2.java
+++ 240116/출석 부르기 2/calling-attendance-2.java
@@ -11,9 +11,9 @@ public class Main {
             }else if(a==2){
                 System.out.println("Tom");
             } else if(a==3){
-                System.out.println("Paul"));
+                System.out.println("Paul");
             } else if(a==4){
-                System.out.println("Sam"));
+                System.out.println("Sam");
             } else{
                 System.out.println("Vaancy");
                 break;
