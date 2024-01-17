b94654c2be232fe15450fa0488609c64006de9db
limch02 <144610436+limch02@users.noreply.github.com>
2024-01-17 14:53:08 +0900

    Title: 합성수 판별기, Time: 85ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240117/합성수 판별기/factorization-discriminator.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st= new StringTokenizer(br.readLine());
+        int a = Integer.parseInt(st.nextToken());
+        boolean k = true;
+        for (int i = 1;i<=a;i++){
+            if (a%i==0){
+                k = false;
+            }
+
+        }
+        if (k){
+            System.out.println("N");
+        }else {
+            System.out.println("C");
+        }
+    }
+}
