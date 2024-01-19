1dc3690e48da37711946f21b8b7fd7ecedbe8ca3
limch02 <144610436+limch02@users.noreply.github.com>
2024-01-19 14:14:51 +0900

    Title: 별표 출력하기, Time: 114ms, Memory: 8MB, Status: Passed - CodeTree

--- 240119/별표 출력하기/print-star.java
+++ 240119/별표 출력하기/print-star.java
@@ -7,13 +7,13 @@ public class Main {
         int a = Integer.parseInt(br.readLine());
         for (int i = 1;i<=a;i++){
             for (int j = 1;j<=i;j++){
-                System.out.print("*");
+                System.out.print("* ");
             }
             System.out.println();
         }
         for (int i = a-1;i>0;i--){
             for (int j = 1;j<=i;j++){
-                System.out.print("*");
+                System.out.print("* ");
             }
             System.out.println();
         }
