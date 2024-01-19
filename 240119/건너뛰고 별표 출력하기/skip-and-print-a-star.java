a89dd8774392acfceb699b0c27d1c91a4a87787e
limch02 <144610436+limch02@users.noreply.github.com>
2024-01-19 14:17:13 +0900

    Title: 건너뛰고 별표 출력하기, Time: 105ms, Memory: 8MB, Status: Passed - CodeTree

--- 240119/건너뛰고 별표 출력하기/skip-and-print-a-star.java
+++ 240119/건너뛰고 별표 출력하기/skip-and-print-a-star.java
@@ -7,15 +7,15 @@ public class Main {
         int a = Integer.parseInt(br.readLine());
         for (int i = 1;i<=a;i++){
             for (int j = 1;j<=i;j++){
-                System.out.println("*");
+                System.out.print("*");
             }
-            System.out.println();
+            System.out.println("\n");
         }
         for (int i = a-1;i>0;i--){
             for (int j = 1;j<=i;j++){
-                System.out.println("*");
+                System.out.print("*");
             }
-            System.out.println();
+            System.out.println("\n");
         }
     }
 }
