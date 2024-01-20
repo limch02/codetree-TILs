6b1d6b0eeff9fc0fa60e9a059a2a06e8c519d459
limch02 <144610436+limch02@users.noreply.github.com>
2024-01-20 12:43:11 +0900

    Title: 별표 출력하기 10, Time: 90ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240120/별표 출력하기 10/print-star-10.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        for(int i = 0;i<2*n;i++){
+            if (i%2==1){
+                for (int j = 0;j<n-((i-1)/2);j++) {
+                    System.out.print("*");
+                }
+                System.out.println();
+            }else {
+                for (int j = 0;j<1+(i/2);j++){
+                    System.out.print("*");
+                }
+                System.out.println();
+            }
+        }
+    }
+}
