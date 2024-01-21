59812e735c6f5f1051280cfbd85efdc34c674909
limch02 <144610436+limch02@users.noreply.github.com>
2024-01-21 14:51:05 +0900

    Title: 구구단 만들기 2, Time: 107ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240121/구구단 만들기 2/print-multiplication-table-2.java
@@ -0,0 +1,27 @@
+import java.io.*;
+    import java.util.*;
+
+    public class Main {
+        public static void main(String[] args) throws IOException {
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int n = Integer.parseInt(st.nextToken());
+            int m = Integer.parseInt(st.nextToken());
+            if (n == m){
+                for (int i = 1;i<=9;i++){
+                    System.out.println(n + " * " + i + " = " + i * n);
+                }
+            }else {
+                for (int i = 1; i <= 9; i++) {
+                    for (int j = m; j >= n; j--) {
+                        if (j != n) {
+                            System.out.print(j + " * " + i + " = " + i * j + " / ");
+                        } else {
+                            System.out.print(j + " * " + i + " = " + i * j);
+                        }
+                    }
+                    System.out.println();
+                }
+            }
+        }
+    }
