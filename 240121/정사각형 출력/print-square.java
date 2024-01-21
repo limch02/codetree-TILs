1e74d04a599a67b9d0daf1413d84b5245e27f979
limch02 <144610436+limch02@users.noreply.github.com>
2024-01-21 14:08:59 +0900

    Title: 정사각형 출력, Time: 186ms, Memory: 11MB, Status: Passed - CodeTree

--- 240121/정사각형 출력/print-square.java
+++ 240121/정사각형 출력/print-square.java
@@ -1,18 +1,18 @@
 import java.io.*;
-import java.util.*;
+    import java.util.*;
 
-public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st.nextToken());
-        int cnt = 1;
-        for(int i = 0 ;i<n;i++){
-            for (int j = 0; j<n;j++){
-                System.out.println(cnt);
-                cnt++;
+    public class Main {
+        public static void main(String[] args) throws IOException {
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int n = Integer.parseInt(st.nextToken());
+            int cnt = 1;
+            for(int i = 0 ;i<n;i++){
+                for (int j = 0; j<n;j++){
+                    System.out.print(cnt+" ");
+                    cnt++;
+                }
+                System.out.println();
             }
-            System.out.println();
         }
     }
-}
