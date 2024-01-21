89647372ee3a502bcbb887946d920162208c4cf7
limch02 <144610436+limch02@users.noreply.github.com>
2024-01-21 14:10:33 +0900

    Title: 1-9 정사각형, Time: 99ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240121/1-9 정사각형/1-9-square.java
@@ -0,0 +1,21 @@
+import java.io.*;
+    import java.util.*;
+
+    public class Main {
+        public static void main(String[] args) throws IOException {
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int n = Integer.parseInt(st.nextToken());
+            int cnt = 1;
+            for(int i = 0 ;i<n;i++){
+                for (int j = 0; j<n;j++){
+                    if (cnt == 10){
+                        cnt = 1;
+                    }
+                    System.out.print(cnt);
+                    cnt++;
+                }
+                System.out.println();
+            }
+        }
+    }
