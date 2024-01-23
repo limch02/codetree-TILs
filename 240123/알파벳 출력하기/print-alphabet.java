9d31936b19f4cb8c389d4a6b55199626b9a22ac7
limch02 <144610436+limch02@users.noreply.github.com>
2024-01-23 13:23:17 +0900

    Title: 알파벳 출력하기, Time: 95ms, Memory: 8MB, Status: Wrong Answer - CodeTree

--- 240123/알파벳 출력하기/print-alphabet.java
+++ 240123/알파벳 출력하기/print-alphabet.java
@@ -8,6 +8,9 @@ import java.io.*;
             int a = Integer.parseInt(st.nextToken());
             int cnt = 65;
             for(int i = 0;i<a;i++){
+                if(cnt == 93){
+                    cnt = 65;
+                }
                 for (int j = 0; j<i+1;j++){
                     System.out.print((char)cnt);
                     cnt++;
