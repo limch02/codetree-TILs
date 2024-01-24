323c4dfb8e80ba3427993c726719b399c7ba49d6
limch02 <144610436+limch02@users.noreply.github.com>
2024-01-24 13:32:24 +0900

    Title: 원소 10개의 합, Time: 113ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240124/원소 10개의 합/sum-of-10-elements.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int sum = 0;
+        for (int i =0;i<10;i++){
+            int a = Integer.parseInt(st.nextToken());
+            sum +=a;
+        }
+        System.out.println(sum);
+    }
+}
