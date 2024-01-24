dba812c6f70ecffdacbbeb03a80ceeafa356e798
limch02 <144610436+limch02@users.noreply.github.com>
2024-01-24 13:59:18 +0900

    Title: 10개 입력받기, Time: 110ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 240124/10개 입력받기/receive-10-inputs.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        double sum =0;
+        double cnt = 0;
+        while (st.hasMoreTokens()){
+            int d = Integer.parseInt(st.nextToken());
+            if (d == 0){
+                break;
+            }else {
+                sum += d;
+                cnt++;
+            }
+        }
+        String s = String.format("%.1f",sum/cnt);
+        System.out.println((int)sum+" "+s);
+    }
+}
