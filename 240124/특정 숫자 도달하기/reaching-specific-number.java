6621195079b7aa1dbde7ccc64f8218521059bd60
limch02 <144610436+limch02@users.noreply.github.com>
2024-01-24 13:42:46 +0900

    Title: 특정 숫자 도달하기, Time: 93ms, Memory: 9MB, Status: Wrong Answer - CodeTree

---
+++ 240124/특정 숫자 도달하기/reaching-specific-number.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        double sum = 0;
+        double cnt = 0;
+        for (int i =0;i<10;i++){
+            int a = Integer.parseInt(st.nextToken());
+            if (a>=250){
+                break;
+            }else {
+                sum += a;
+                cnt++;
+            }
+        }
+        String s = String.format("%.1f",sum/5);
+        System.out.println((int)sum+" "+s);
+    }
+}
