31f908218c8a27697b8b32722eef7112fc0c8731
limch02 <144610436+limch02@users.noreply.github.com>
2024-01-24 14:22:12 +0900

    Title: 나머지의 재귀함수, Time: 100ms, Memory: 8MB, Status: Passed - CodeTree

--- 240124/나머지의 재귀함수/recursive-function-of-remainder.java
+++ 240124/나머지의 재귀함수/recursive-function-of-remainder.java
@@ -2,15 +2,24 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static int abc(int a){
-        if (a==1){
+    private static Map<Integer, Integer> memo = new HashMap<>();
+
+    public static int abc(int a) {
+        if (a == 1) {
             return 2;
-        } else if (a==2) {
+        } else if (a == 2) {
             return 4;
-        }else {
-            return (abc(a-1)*abc(a-2))%100;
+        } else {
+            if (memo.containsKey(a)) {
+                return memo.get(a);
+            } else {
+                int result = (abc(a - 1) * abc(a - 2)) % 100;
+                memo.put(a, result);
+                return result;
+            }
         }
     }
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int a = Integer.parseInt(br.readLine());
