613b66689336ae38f0f25ac9d3854b6657d57b04
limch02 <144610436+limch02@users.noreply.github.com>
2024-01-25 14:59:13 +0900

    Title: 배열의 특정 위치 찾기 3, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240125/배열의 특정 위치 찾기 3/find-specific-location-fo-array-3.java
+++ 240125/배열의 특정 위치 찾기 3/find-specific-location-fo-array-3.java
@@ -1,17 +1,19 @@
 import java.io.*;
+import java.util.ArrayList;
 import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st= new StringTokenizer(br.readLine());
-        int [] arr = new int[10];
+        ArrayList<Integer> arr = new ArrayList<>();
         int sum =0;
-        for (int i = 0 ; i<10;i++){
+        int i =0;
+        while (st.hasMoreTokens()){
             int k =Integer.parseInt(st.nextToken());
-            arr[i] =k;
+            arr.add(k);
             if (k == 0){
-                sum = arr[i-1]+arr[i-2]+arr[i-3];
+                sum = arr.get(i-1)+arr.get(i-2)+arr.get(i-3);
                 break;
             }
         }
