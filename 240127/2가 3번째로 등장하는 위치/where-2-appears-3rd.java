d5400272133f7f93cad99272e684540282e236e8
limch02 <144610436+limch02@users.noreply.github.com>
2024-01-27 14:04:42 +0900

    Title: 2가 3번째로 등장하는 위치, Time: 91ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240127/2가 3번째로 등장하는 위치/where-2-appears-3rd.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int a = Integer.parseInt(br.readLine());
+        StringTokenizer st= new StringTokenizer(br.readLine());
+        int cnt=0;
+        for (int i =0;i<a;i++){
+            int b = Integer.parseInt(st.nextToken());
+            if (b==2){
+                cnt++;
+            }
+            if (cnt==3){
+                System.out.println(i+1);
+            }
+        }
+    }
+}
