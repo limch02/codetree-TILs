06d58297efb9dbc0f8b6f1465596e1b288fa7a31
limch02 <144610436+limch02@users.noreply.github.com>
2024-01-27 14:14:07 +0900

    Title: n개의 숫자 중 최소, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240127/n개의 숫자 중 최소/min-of-n-num.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int a = Integer.parseInt(br.readLine());
+        StringTokenizer st= new StringTokenizer(br.readLine());
+        int [] arr = new int[10];
+        int max =Integer.MAX_VALUE;
+        int cnt =0;
+        for (int i =0;i<a;i++){
+            int b = Integer.parseInt(st.nextToken());
+            arr[i]=b;
+            if (b<max){
+                max = b;
+            }
+        }
+        for (int i =0;i<a;i++){
+            if (arr[i]==max){
+                cnt++;
+            }
+        }
+        System.out.println(max+" "+cnt);
+    }
+}
