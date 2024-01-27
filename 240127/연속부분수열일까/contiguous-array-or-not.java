8a07b673704e36a59953a501f45148cc6dea9a9c
limch02 <144610436+limch02@users.noreply.github.com>
2024-01-27 14:01:38 +0900

    Title: 연속부분수열일까, Time: 104ms, Memory: 8MB, Status: Passed - CodeTree

--- 240127/연속부분수열일까/contiguous-array-or-not.java
+++ 240127/연속부분수열일까/contiguous-array-or-not.java
@@ -4,39 +4,58 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        // Read input sizes
         StringTokenizer st = new StringTokenizer(br.readLine());
         int n = Integer.parseInt(st.nextToken());
         int q = Integer.parseInt(st.nextToken());
+
+        // Read sequences A and B
+        int[] arr1 = new int[n];
+        int[] arr2 = new int[q];
+
         StringTokenizer s1 = new StringTokenizer(br.readLine());
         StringTokenizer s2 = new StringTokenizer(br.readLine());
-        int [] arr1 = new int[n];
-        int [] arr2 = new int[q];
-        for (int i = 0;i<n;i++){
-            int c= Integer.parseInt(s1.nextToken());
-            arr1[i] = c;
+
+        for (int i = 0; i < n; i++) {
+            arr1[i] = Integer.parseInt(s1.nextToken());
         }
-        for (int i = 0;i<q;i++){
-            int c= Integer.parseInt(s2.nextToken());
-            arr2[i] = c;
+
+        for (int i = 0; i < q; i++) {
+            arr2[i] = Integer.parseInt(s2.nextToken());
         }
-        boolean t = true;
-        for (int i = 0 ;i<n;i++) {
-            if (arr1[i]==arr2[0]) {
-                for (int j =0;j<q;j++){
-                    if (arr1[i+j]!=arr2[j]) {
-                        t = false;
-                        break;
-                    }
-                }
-                break;
-            }else {
-                t = false;
-            }
-        }
-        if (t){
+
+        // Check if B is a subsequence of A
+        boolean isSubsequence = checkSubsequence(arr1, arr2);
+
+        // Output the result
+        if (isSubsequence) {
             System.out.println("Yes");
-        }else {
+        } else {
             System.out.println("No");
         }
+    }
+
+    private static boolean checkSubsequence(int[] arr1, int[] arr2) {
+        // Iterate through A to find the starting point of a potential subsequence
+        for (int i = 0; i <= arr1.length - arr2.length; i++) {
+            boolean isMatch = true;
+
+            // Check if the current position in A forms a subsequence with B
+            for (int j = 0; j < arr2.length; j++) {
+                if (arr1[i + j] != arr2[j]) {
+                    isMatch = false;
+                    break;
+                }
+            }
+
+            // If a match is found, return true
+            if (isMatch) {
+                return true;
+            }
+        }
+
+        // No matching subsequence found
+        return false;
     }
 }
