c0941682e406efc7f920f1535f72e4263ccaf085
limch02 <144610436+limch02@users.noreply.github.com>
2024-02-01 13:23:25 +0900

    Title: 가장 짧은 문자열, Time: 93ms, Memory: 8MB, Status: Passed - CodeTree

--- 240201/가장 짧은 문자열/shortest-string.java
+++ 240201/가장 짧은 문자열/shortest-string.java
@@ -5,12 +5,16 @@ public class Main {
     public static void main(String[] args) throws  IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
         for (int i = 0;i<3;i++){
             String s = br.readLine();
             if (min > s.length()){
                 min = s.length();
             }
+            if (max<s.length()){
+                max = s.length();
+            }
         }
-        System.out.println(min);
+        System.out.println(max-min);
     }
 }
