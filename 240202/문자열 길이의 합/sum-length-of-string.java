7cdc60b88e76b83295c5ca94dc4ff1c79bbcd18a
limch02 <144610436+limch02@users.noreply.github.com>
2024-02-02 13:16:54 +0900

    Title: 문자열 길이의 합, Time: 100ms, Memory: 8MB, Status: Passed - CodeTree

--- 240202/문자열 길이의 합/sum-length-of-string.java
+++ 240202/문자열 길이의 합/sum-length-of-string.java
@@ -11,11 +11,11 @@ public class Main {
         for (int i = 0;i<a;i++){
             String s = br.readLine();
             sum += s.length();
-            for (int j = 0;j<s.length();j++) {
-                if (String.valueOf(s.charAt(j)).equals("a")) {
+           
+                if (String.valueOf(s.charAt(0)).equals("a")) {
                     cnt++;
                 }
-            }
+            
         }
         sb.append(sum).append(" ").append(cnt);
         System.out.println(sb);
