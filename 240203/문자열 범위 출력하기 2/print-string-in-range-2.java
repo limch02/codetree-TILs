cf0eb5f2c46f39026ace5c70f67a7b0e279c1b82
limch02 <144610436+limch02@users.noreply.github.com>
2024-02-03 00:09:58 +0900

    Title: 문자열 범위 출력하기 2, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240203/문자열 범위 출력하기 2/print-string-in-range-2.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws  IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s = br.readLine();
+        int a = Integer.parseInt(br.readLine());
+        for (int i =0;i<a;i++){
+            System.out.print(s.charAt(s.length()-1-i));
+        }
+    }
+}
