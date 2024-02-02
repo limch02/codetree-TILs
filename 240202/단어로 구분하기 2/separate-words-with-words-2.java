5ad23eec2a391287d01c51825fbfd03a29187056
limch02 <144610436+limch02@users.noreply.github.com>
2024-02-02 13:20:20 +0900

    Title: 단어로 구분하기 2, Time: 107ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240202/단어로 구분하기 2/separate-words-with-words-2.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws  IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        String s []= br.readLine().split(" ");
+        for (int i = 0;i<s.length;i++){
+            if (i%2==0) {
+                sb.append(s[i]).append("\n");
+            }
+        }
+        System.out.println(sb);
+    }
+}
