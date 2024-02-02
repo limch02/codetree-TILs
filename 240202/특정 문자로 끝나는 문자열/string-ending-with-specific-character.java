1e3abb9680b08daf6868edc80c8a7faf5b56cb0f
limch02 <144610436+limch02@users.noreply.github.com>
2024-02-02 13:25:21 +0900

    Title: 특정 문자로 끝나는 문자열, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240202/특정 문자로 끝나는 문자열/string-ending-with-specific-character.java
@@ -0,0 +1,25 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws  IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        String s []= new String[10];
+        for (int i = 0 ;i<10;i++){
+            s[i] = br.readLine();
+        }
+        String k = br.readLine();
+        for (int i = 0;i<10;i++){
+            String t = s[i];
+            if (k.equals(String.valueOf(t.charAt(t.length()-1)))) {
+                sb.append(s[i]).append("\n");
+            }
+        }
+        if (sb.isEmpty()){
+            System.out.println("None");
+        }else {
+            System.out.println(sb);
+        }
+    }
+}
