2653b54b8415e222f4a80664941e1e7f8fee1660
limch02 <144610436+limch02@users.noreply.github.com>
2024-02-05 17:47:37 +0900

    Title: 부분문자열 위치 구하기, Time: 91ms, Memory: 8MB, Status: Wrong Answer - CodeTree

--- 240205/부분문자열 위치 구하기/find-location-of-substring.java
+++ 240205/부분문자열 위치 구하기/find-location-of-substring.java
@@ -6,33 +6,31 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String s = br.readLine();
         String b = br.readLine();
-        int cnt =0;
-        if (s.equals(b)){
-            System.out.println(0);
-        }else {
-            for (int i = 0; i < s.length(); i++) {
-                if (i == s.length() - b.length() + 1) {
-                    break;
-                } else {
-                    boolean bo = false;
-                    if (s.charAt(i) == b.charAt(0)) {
-                        for (int j = 0; j < b.length(); j++) {
-                            if (s.charAt(i + j) == b.charAt(j)) {
-                                bo = true;
-                            } else {
-                                bo = false;
-                            }
+        boolean bo = false;
+        int p = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (i == s.length() - b.length() + 1) {
+                break;
+            } else {
+                if (s.charAt(i) == b.charAt(0)) {
+                    for (int j = 0; j < b.length(); j++) {
+                        if (s.charAt(i + j) == b.charAt(j)) {
+                            bo = true;
+                        } else {
+                            bo = false;
                         }
                     }
-                    if (bo) {
-                        cnt++;
-                    }
                 }
-            }if (cnt==0){
-                System.out.println(-1);
-            }else {
-                System.out.println(cnt);
+                if (bo){
+                    p = i;
+                    break;
+                }
             }
+        }
+        if (bo) {
+            System.out.println(p);
+        }else{
+            System.out.println(-1);
         }
     }
 }
