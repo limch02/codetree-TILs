6ef26dc34eb8f75f5b21d0de0c8eab6673534153
limch02 <144610436+limch02@users.noreply.github.com>
2024-02-05 17:56:56 +0900

    Title: 부분 문자열의 개수, Time: 84ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240205/부분 문자열의 개수/number-of-substrings.java
@@ -0,0 +1,35 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s = br.readLine();
+        String b = br.readLine();
+        int cnt =0;
+        for (int i = 0 ; i<s.length();i++){
+            if (i==s.length()-b.length()+1){
+                break;
+            }else {
+                boolean bo = false;
+                if (s.charAt(i)==b.charAt(0)) {
+                    for (int j = 0; j < b.length(); j++) {
+                        if (s.charAt(i+j)==b.charAt(j)){
+                            bo = true;
+                        }else {
+                            bo = false;
+                        }
+                    }
+                }
+                if (bo) {
+                    cnt++;
+                }
+            }
+        }
+        if (cnt==0){
+            System.out.println(-1);
+        }else {
+            System.out.println(cnt);
+        }
+    }
+}
