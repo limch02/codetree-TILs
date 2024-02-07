c81f689ad8754d07cde90eda69c3df65aad1e229
limch02 <144610436+limch02@users.noreply.github.com>
2024-02-07 13:32:05 +0900

    Title: 문자열 한 칸씩 밀어내며 뒤집기, Time: 232ms, Memory: 17MB, Status: Passed - CodeTree

--- 240207/문자열 한 칸씩 밀어내며 뒤집기/shift-reverse-string.java
+++ 240207/문자열 한 칸씩 밀어내며 뒤집기/shift-reverse-string.java
@@ -5,29 +5,23 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        StringBuffer sb = new StringBuffer();
         String s = st.nextToken();
         int a = Integer.parseInt(st.nextToken());
-        if (s.length()==1){
-            for(int i =0;i<a;i++){
+        for (int i =0;i<a;i++){
+            int n = Integer.parseInt(br.readLine());
+            if (n == 1){
+                s = s.substring(1,s.length())+s.charAt(0);
+                System.out.println(s);
+            } else if (n==2) {
+                s = s.charAt(s.length()-1)+s.substring(0,s.length()-1);
+                System.out.println(s);
+            }else{
+                StringBuffer sb = new StringBuffer(s);
+                sb.reverse();
+                s = sb.toString();
                 System.out.println(s);
             }
-        }else{
-            for (int i =0;i<a;i++){
-                int n = Integer.parseInt(br.readLine());
-                if (n == 1){
-                    s = s.substring(1,s.length())+s.charAt(0);
-                    System.out.println(s);
-                } else if (n==2) {
-                    s = s.charAt(s.length()-1)+s.substring(0,s.length()-1);
-                    System.out.println(s);
-                }else{
-                    sb.append(s);
-                    sb.reverse();
-                    s = sb.toString();
-                    System.out.println(s);
-                }
-            }
         }
+
     }
 }
