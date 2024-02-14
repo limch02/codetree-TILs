0d631b59e0ce59322fd9922859695c3466026c7e
limch02 <144610436+limch02@users.noreply.github.com>
2024-02-14 13:57:33 +0900

    Title: 두 직사각형, Time: 93ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240214/두 직사각형/two-rectangles.java
@@ -0,0 +1,30 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int [][] arr = new int[101][101];
+        boolean t = false;
+        for (int i =0;i<2;i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int n1 = Integer.parseInt(st.nextToken());
+            int m1 = Integer.parseInt(st.nextToken());
+            int n2 = Integer.parseInt(st.nextToken());
+            int m2 = Integer.parseInt(st.nextToken());
+            for (int j=n1+1;j<n2;j++){
+                for (int k = m1+1;k<m2;k++){
+                    if (arr[j][k]>=1){
+                        t = true;
+                    }
+                    arr[j][k]++;
+                }
+            }
+        }
+        if (t){
+            System.out.println("overlapping");
+        }else {
+            System.out.println("nonoverlapping");
+        }
+    }
+}
