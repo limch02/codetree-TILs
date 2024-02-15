7492333fc292b736e69f793b54f6375a1c26879b
limch02 <144610436+limch02@users.noreply.github.com>
2024-02-15 19:07:43 +0900

    Title: 전부 겹치는 선분 2, Time: 104ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240215/전부 겹치는 선분 2/overlapping-line-segments-2.java
@@ -0,0 +1,29 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int a = Integer.parseInt(br.readLine());
+        int [] arr = new int[101];
+        for (int i =0;i<a;i++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int n = Integer.parseInt(st.nextToken());
+            int m = Integer.parseInt(st.nextToken());
+            for (int j = n;j<=m;j++){
+                arr[j]++;
+            }
+        }
+        boolean t = false;
+        for (int i =0;i<arr.length;i++){
+            if (arr[i] == a-1){
+                t = true;
+            }
+        }
+        if (t) {
+            System.out.println("Yes");
+        }else{
+            System.out.println("No");
+        }
+    }
+}
