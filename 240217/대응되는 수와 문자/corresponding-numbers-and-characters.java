20b1c05f204bd4c71532f8100b043b6c3d12c047
limch02 <144610436+limch02@users.noreply.github.com>
2024-02-17 14:12:23 +0900

    Title: 대응되는 수와 문자, Time: 3031ms, Memory: 24MB, Status: Time Limit Exceed - CodeTree

--- 240217/대응되는 수와 문자/corresponding-numbers-and-characters.java
+++ 240217/대응되는 수와 문자/corresponding-numbers-and-characters.java
@@ -2,29 +2,35 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-    public static boolean isNumberic(String str) {
-        return str.chars().allMatch(Character::isDigit);
+    public static boolean isNumeric(String str) {
+        return str.matches("\\d+");
     }
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st= new StringTokenizer(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
         int a = Integer.parseInt(st.nextToken());
         int b = Integer.parseInt(st.nextToken());
-        Map<String,Integer> map= new HashMap<>();
-        for (int i =0;i<a;i++){
+
+        Map<String, Integer> map = new HashMap<>();
+        for (int i = 0; i < a; i++) {
             StringTokenizer s = new StringTokenizer(br.readLine());
             String k = s.nextToken();
-            map.put(k,i+1);
+            map.put(k, i+1);
         }
-        for (int i = 0;i<b;i++){
-            String t =br.readLine();
-            if (isNumberic(t)){
-                for (String string : map.keySet()){
-                    if (map.get(string)==Integer.valueOf(t)){
-                        System.out.println(string);
+
+        Set<String> keySet = map.keySet(); // 키셋을 한 번만 가져옴
+        for (int i = 0; i < b; i++) {
+            String t = br.readLine();
+            if (isNumeric(t)) {
+                int targetValue = Integer.parseInt(t);
+                for (String key : keySet) { // 키셋을 반복하여 해당 값 찾음
+                    if (map.get(key) == targetValue) {
+                        System.out.println(key);
+                        break; // 찾았으면 반복 종료
                     }
                 }
-            }else {
+            } else {
                 System.out.println(map.get(t));
             }
         }
