490001b956351511418cf43fa9a310f7226816e0
limch02 <144610436+limch02@users.noreply.github.com>
2024-02-21 13:54:58 +0900

    Title: 차이가 가장 작은 수, Time: 93ms, Memory: 8MB, Status: Wrong Answer - CodeTree

--- 240221/차이가 가장 작은 수/the-number-with-the-smallest-difference.java
+++ 240221/차이가 가장 작은 수/the-number-with-the-smallest-difference.java
@@ -6,24 +6,25 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         int a = Integer.parseInt(st.nextToken());
-        int b= Integer.parseInt(st.nextToken());
-        int [] arr = new int[a];
-        TreeSet<Integer> set = new TreeSet<>();
-        for (int i =0;i<a;i++){
+        int b = Integer.parseInt(st.nextToken());
+        int[] arr = new int[a];
+        ArrayList<Integer> diffs = new ArrayList<>();
+        for (int i = 0; i < a; i++) {
             arr[i] = Integer.parseInt(br.readLine());
         }
-        for (int i =0;i<a;i++){
-            for (int j =i+1;j<a;j++){
-                int k = (int)Math.abs(arr[j]-arr[i]);
-                if (k>=b){
-                    set.add(k);
-                }
+        Arrays.sort(arr);
+        for (int i = 0; i < a - 1; i++) {
+            int diff = Math.abs(arr[i + 1] - arr[i]);
+            if (diff >= b) {
+                diffs.add(diff);
             }
         }
-        if (set.isEmpty()){
+        Collections.sort(diffs);
+
+        if (diffs.isEmpty()) {
             System.out.println(-1);
-        }else {
-            System.out.println(set.first());
+        } else {
+            System.out.println(diffs.get(0));
         }
     }
 }
