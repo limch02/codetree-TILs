f66147d8e419073cb579e32e67147165f9c88b1e
limch02 <144610436+limch02@users.noreply.github.com>
2024-02-22 14:05:44 +0900

    Title: 정수 명령어, Time: 638ms, Memory: 31MB, Status: Wrong Answer - CodeTree

---
+++ 240222/정수 명령어/Integer-command.java
@@ -0,0 +1,35 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int a = Integer.parseInt(br.readLine());
+        for (int i =0;i<a;i++){
+            int b = Integer.parseInt(br.readLine());
+            ArrayList<Integer> arr = new ArrayList<>();
+            for (int j = 0;j<b;j++){
+                StringTokenizer st = new StringTokenizer(br.readLine());
+                String s = st.nextToken();
+                int c = Integer.parseInt(st.nextToken());
+                if (s.equals("I")) {
+                    arr.add(c);
+                }else {
+                    Collections.sort(arr);
+                    if (!arr.isEmpty()) {
+                        if (c == 1) {
+                            arr.remove(arr.size() - 1);
+                        } else {
+                            arr.remove(0);
+                        }
+                    }
+                }
+            }
+            if (arr.isEmpty()){
+                System.out.println("EMPTY");
+            }else{
+                System.out.println(arr.get(arr.size()-1)+" " +arr.get(0));
+            }
+        }
+    }
+}
