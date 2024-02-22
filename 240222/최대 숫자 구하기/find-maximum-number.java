8d81518bec9563f84b74e8be3cf8a1de44de854d
limch02 <144610436+limch02@users.noreply.github.com>
2024-02-22 14:24:05 +0900

    Title: 최대 숫자 구하기, Time: 526ms, Memory: 19MB, Status: Wrong Answer - CodeTree

---
+++ 240222/최대 숫자 구하기/find-maximum-number.java
@@ -0,0 +1,26 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        ArrayList<Integer> arr =new ArrayList<>();
+        StringTokenizer s = new StringTokenizer(br.readLine());
+        for (int i=1;i<=b;i++){
+            arr.add(i);
+        }
+        for (int i = 0;i<a;i++){
+            int c = Integer.parseInt(s.nextToken());
+            if (c>arr.size()){
+                arr.remove(arr.size()-1);
+            }else {
+                arr.remove(c-1);
+            }
+            Collections.sort(arr);
+            System.out.println(arr.get(arr.size()-1));
+        }
+    }
+}
