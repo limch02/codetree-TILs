b61a91f412fb48256b98dc701bced5dc4558eae8
limch02 <144610436+limch02@users.noreply.github.com>
2024-02-23 21:51:22 +0900

    Title: 최대로 연속한 숫자, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240223/최대로 연속한 숫자/maximum-consecutive-number.java
+++ 240223/최대로 연속한 숫자/maximum-consecutive-number.java
@@ -7,32 +7,27 @@ public class Main {
         StringTokenizer st = new StringTokenizer(br.readLine());
         int a = Integer.parseInt(st.nextToken());
         int b = Integer.parseInt(st.nextToken());
-        TreeSet<Integer> set = new TreeSet<>();
-        for (int i =0;i<=a;i++){
-            set.add(i); //수열 생성
-        }
-        StringTokenizer s = new StringTokenizer(br.readLine()); //제거할 수
-        for (int i =0;i<b;i++){
-            set.remove(Integer.parseInt(s.nextToken()));
-            Iterator<Integer> it = set.iterator();
-            int len = 1;
-            int max = Integer.MIN_VALUE;
-            int [] arr = new int[set.size()];
-            int j =0;
-            while (it.hasNext()){
-                arr[j] = it.next();
-                j++;
-            }
-            for (int k =0;k<arr.length-1;k++){
-                if (arr[k]+1==arr[k+1]) {
-                    len++;
-                } else{
-                    len =1;
-                }
-                if (max<len){
-                    max = len;
-                }
 
+        boolean[] sequence = new boolean[a + 1]; // 숫자가 수열에 속하는지 여부를 저장할 배열
+
+        for (int i = 0; i <= a; i++) {
+            sequence[i] = true; // 모든 숫자를 수열에 포함된 것으로 초기화
+        }
+
+        StringTokenizer s = new StringTokenizer(br.readLine());
+
+        for (int i = 0; i < b; i++) {
+            int removed = Integer.parseInt(s.nextToken());
+            sequence[removed] = false;
+            int max = 0;
+            int current = 0;
+            for (int j = 0; j <= a; j++) {
+                if (sequence[j]) {
+                    current++;
+                } else {
+                    current = 0;
+                }
+                max = Math.max(max, current);
             }
             System.out.println(max);
         }
