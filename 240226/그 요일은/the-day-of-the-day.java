b7393285f4c4324ae39b230d6c1927c09e2d0ffd
limch02 <144610436+limch02@users.noreply.github.com>
2024-02-26 22:00:16 +0900

    Title: 그 요일은, Time: 97ms, Memory: 8MB, Status: Passed - CodeTree

--- 240226/그 요일은/the-day-of-the-day.java
+++ 240226/그 요일은/the-day-of-the-day.java
@@ -1,53 +1,51 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Scanner;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-    public static int numOfDays(int m, int d) {
-        // 계산 편의를 위해 월마다 몇 일이 있는지를 적어줍니다.
-        int[] days = new int[]{0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-        int totalDays = 0;
-
-        // 1월부터 (m - 1)월 까지는 전부 꽉 채워져 있습니다.
-        for(int i = 1; i < m; i++)
-            totalDays += days[i];
-
-        // m월의 경우에는 정확이 d일만 있습니다.
-        totalDays += d;
-
-        return totalDays;
-    }
-
     public static void main(String[] args) throws IOException {
-        BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st= new StringTokenizer(br.readLine());
-        // 변수 선언 및 입력
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+
+        int[] days = new int[]{0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+        String[] week = new String[]{"Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"};
+        st = new StringTokenizer(br.readLine(), " ");
         int m1 = Integer.parseInt(st.nextToken());
         int d1 = Integer.parseInt(st.nextToken());
         int m2 = Integer.parseInt(st.nextToken());
         int d2 = Integer.parseInt(st.nextToken());
-        String s = br.readLine();
-        // 두 날짜간의 차이가 몇 일인지를 구합니다.
-        int diff = numOfDays(m2, d2) - numOfDays(m1, d1)+1;
+        String str = br.readLine();
 
-        int cnt =0;
-        if (s.equals("Mon")){
-            System.out.println(diff/7+1);
-        } else if (s.equals("Tue")) {
-            System.out.println((diff+6)/7);
-        }else if (s.equals("Wed")) {
-            System.out.println((diff+5)/7);
-        }else if (s.equals("Thu")) {
-            System.out.println((diff+4)/7);
-        }else if (s.equals("Fri")) {
-            System.out.println((diff+3)/7);
-        }else if (s.equals("Sat")) {
-            System.out.println((diff+2)/7);
-        }else {
-            System.out.println((diff+1)/7);
+        int sum1 = 0, sum2 = 0;
+        for (int i = 1; i < m1; i++) {
+            sum1 += days[i];
+        }
+        for (int i = 1; i < m2; i++) {
+            sum2 += days[i];
+        }
+        sum1 += d1;
+        sum2 += d2;
+
+        int diff = sum2 - sum1;
+        int start = 0, target = 0, answer = 0;
+
+        for (int i = 1; i < week.length; i++) {
+            if (str.equals(week[i])) {
+                target = i;
+                break;
+            }
         }
 
+        for (int i = 1; i <= diff; i++) {
+            start = (start + 1) % 7;
+            if (start == target) {
+                answer++;
+            }
+        }
+        bw.write(String.valueOf(answer));
+
+        br.close();
+        bw.flush();
+        bw.close();
     }
 }
