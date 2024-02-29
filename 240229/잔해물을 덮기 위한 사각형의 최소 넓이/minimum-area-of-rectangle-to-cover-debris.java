d5a37f925e29aeb384a5d207854d901fd1589e7d
limch02 <144610436+limch02@users.noreply.github.com>
2024-02-29 14:21:18 +0900

    Title: 잔해물을 덮기 위한 사각형의 최소 넓이, Time: 217ms, Memory: 26MB, Status: Wrong Answer - CodeTree

--- 240229/잔해물을 덮기 위한 사각형의 최소 넓이/minimum-area-of-rectangle-to-cover-debris.java
+++ 240229/잔해물을 덮기 위한 사각형의 최소 넓이/minimum-area-of-rectangle-to-cover-debris.java
@@ -7,6 +7,10 @@ public class Main {
         int [][] arr = new int[2001][2001];
         int s1 = 0;
         int s2 = 0;
+        int xmax = Integer.MIN_VALUE;
+        int ymax = Integer.MIN_VALUE;
+        int xmin = Integer.MAX_VALUE;
+        int ymin = Integer.MAX_VALUE;
         for (int i =0;i<2;i++){
             StringTokenizer st = new StringTokenizer(br.readLine());
             int x1 = Integer.parseInt(st.nextToken())+1000;
@@ -23,8 +27,8 @@ public class Main {
                     }
                 }
             }else {
-                for (int j = x1; j <= x2; j++) {
-                    for (int k = y1; k <= y2; k++) {
+                for (int j = x1; j < x2; j++) {
+                    for (int k = y1; k < y2; k++) {
                         if (arr[j][k] == 1) {
                             arr[j][k] = 0;
                             s2++;
@@ -36,10 +40,6 @@ public class Main {
         if (s1 == s2){
             System.out.println(0);
         }else{
-            int xmax = Integer.MIN_VALUE;
-            int ymax = Integer.MIN_VALUE;
-            int xmin = Integer.MAX_VALUE;
-            int ymin = Integer.MAX_VALUE;
             for (int i =0;i<2000;i++){
                 for (int j =0;j<2000;j++){
                     if (arr[i][j]==1&&xmax<i){
