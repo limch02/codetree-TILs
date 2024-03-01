f5e4163fedd1dea24fa98f086a2f04531616d7ef
limch02 <144610436+limch02@users.noreply.github.com>
2024-03-01 13:40:38 +0900

    Title: 연속되는 수 2, Time: 91ms, Memory: 9MB, Status: Wrong Answer - CodeTree

--- 240301/연속되는 수 2/continuous-number2.java
+++ 240301/연속되는 수 2/continuous-number2.java
@@ -1,17 +1,23 @@
 import java.io.*;
+import java.util.*;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int a =Integer.parseInt(br.readLine());
-        int cnt =0;
+        int cnt =1;
+        int max = 0;
         int last = -1;
         for (int i =0;i<a;i++){
             int b = Integer.parseInt(br.readLine());
             if (b!=last){
-                cnt++;
                 last = b;
+                if (cnt > max){
+                    max = cnt;
+                }
+            }else {
+                cnt++;
             }
         }
         System.out.println(cnt);
