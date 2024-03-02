b013ecc08b6bd51b829ce2692884b95199cd70ec
limch02 <144610436+limch02@users.noreply.github.com>
2024-03-02 13:22:57 +0900

    Title: 연속되는 수 4, Time: 96ms, Memory: 9MB, Status: Wrong Answer - CodeTree

---
+++ 240302/연속되는 수 4/continuous-number4.java
@@ -0,0 +1,35 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int a =Integer.parseInt(br.readLine());
+        int cnt = 0;
+        int max = 0;
+        int bigger =0;
+        if (a ==1 ){
+            int n = Integer.parseInt(br.readLine());
+            System.out.println(1);
+        }else {
+            for (int i = 0; i < a; i++) {
+                int b = Integer.parseInt(br.readLine());
+                if (b>bigger){
+                    cnt++;
+                    bigger = b;
+                }else {
+                    if (cnt>max){
+                        max = cnt;
+                    }
+                    bigger =0;
+                    cnt =1;
+                }
+            }
+            if (cnt>max){
+                max = cnt;
+            }
+            System.out.println(max);
+        }
+    }
+}
