921ee31a168932da1cc3ce3d57419df81ad414dd
limch02 <144610436+limch02@users.noreply.github.com>
2024-03-02 13:18:26 +0900

    Title: 연속되는 수 3, Time: 101ms, Memory: 9MB, Status: Wrong Answer - CodeTree

--- 240302/연속되는 수 3/continuous-number3.java
+++ 240302/연속되는 수 3/continuous-number3.java
@@ -9,31 +9,36 @@ public class Main {
         int cnt = 0;
         int max = 0;
         boolean nop = true;
-        for (int i =0;i<a;i++){
-            int b = Integer.parseInt(br.readLine());
-            boolean t;
-            if (b<0){
-                t= false;
-            }else {
-                t = true;
-            }
-            if (t==nop){
-                cnt++;
-            }else{
-                if (nop){
-                    nop = false;
-                }else {
-                    nop = true;
+        if (a ==1 ){
+            int n = Integer.parseInt(br.readLine());
+            System.out.println(1);
+        }else {
+            for (int i = 0; i < a; i++) {
+                int b = Integer.parseInt(br.readLine());
+                boolean t;
+                if (b < 0) {
+                    t = false;
+                } else {
+                    t = true;
                 }
-                if (cnt>max){
-                    max = cnt;
+                if (t == nop) {
+                    cnt++;
+                } else {
+                    if (nop) {
+                        nop = false;
+                    } else {
+                        nop = true;
+                    }
+                    if (cnt > max) {
+                        max = cnt;
+                    }
+                    cnt = 0;
                 }
-                cnt = 0;
             }
+            if (cnt>max){
+                max = cnt;
+            }
+            System.out.println(max);
         }
-        if (cnt>max){
-            max = cnt;
-        }
-        System.out.println(max);
     }
 }
